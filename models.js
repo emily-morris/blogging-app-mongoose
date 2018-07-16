6363991a2bcf1652c3ca598e4b6949e4c0f36130
Emily Morris <morris.emilyanne@gmail.com>
2018-07-15 19:20:50 -0500

    created get, post, put, delete endpoints

--- models.js
+++ models.js
@@ -2,30 +2,29 @@
 
 const mongoose = require("mongoose");
 
-//schema to represent blog post
-const blogSchema = mongoose.Schema({
+//schema to represent posts
+const postSchema = mongoose.Schema({
 	title: { type: String, required: true },
 	content: { type: String, required: true },
 	author: { 
 		firstName: { type:  String, required: true },
 		lastName: { type:  String, required: true }
-	},
-	created: { type: Date, required: true }
+	}
 });
 
-blogSchema.virtual("authorName").get(function() {
-	return `${this.author.firstName} ${this.author.lastName}`.trim();
+postSchema.virtual("authorName").get(function() {
+	return `${this.author.firstName} ${this.author.lastName}`;
 });
 
-blogSchema.methods.serialize = function() {
+//returns an obj that only exposes some of the fields
+postSchema.methods.serialize = function() {
 	return {
 		title: this.title,
 		content: this.content,
 		author: this.authorName,
-		created: this.created
 	};
 };
 
-const Blog = mongoose.model("Blog", blogSchema);
+const Post = mongoose.model("Post", postSchema);
 
-module.exports = { Blog };
+module.exports = { Post };
